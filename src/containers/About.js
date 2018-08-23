4709d246bf9d872a8213724c2bd8f3314444eb43
szhang5 <shiyun.zhang.zoey@gmail.com>
2018-08-23 00:30:23 -0400

    about page

---
+++ src/containers/About.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Icon } from 'react-icons-kit';
+import { blackTie } from 'react-icons-kit/fa/blackTie';
+import { graduationCap } from 'react-icons-kit/fa/graduationCap';
+import { wrench } from 'react-icons-kit/fa/wrench';
+import { thumbsOUp } from 'react-icons-kit/fa/thumbsOUp';
+import { trophy } from 'react-icons-kit/fa/trophy';
+import Header from '../components/Header';
+import gridStyles from '../assets/styles/grid.css';
+import styles from './styles/about.css';
+
+
+const About = ({ data: { loading, resumes } }) => {
+  if (!loading) {
+    return (
+      <div className={[gridStyles.col, gridStyles['span-3-of-4']].join(' ')}>
+        <div className={styles.landingDiv}>
+          <Header />
+          {resumes.map(resume => (
+            <div key={resume.id} className={styles.content}>
+              <div className={gridStyles.row}>
+                <div className={[gridStyles.col, gridStyles['span-1-of-11']].join(' ')}>
+                </div>
+                <div className={[gridStyles.col, gridStyles['span-10-of-11']].join(' ')}>
+                  <article className={styles.leftContent}>
+                    <div className={styles.section}>
+                      <h2>
+                        <div className={styles.blackTie}>
+                          <span>
+                            <Icon size={28} icon={blackTie} />
+                          </span>
+                          {resume.work}
+                        </div>
+                      </h2>
+                      <p dangerouslySetInnerHTML={{ __html: resume.workContent }} />
+                    </div>
+                    <div className={styles.section}>
+                      <h2>
+                        <div className={styles.graduationCap}>
+                          <span>
+                            <Icon size={28} icon={graduationCap} />
+                          </span>
+                          {resume.education}
+                        </div>
+                      </h2>
+                      <p dangerouslySetInnerHTML={{ __html: resume.educationContent }} />
+                    </div>
+                    <div className={styles.section}>
+                      <h2>
+                        <div className={styles.wrench}>
+                          <span>
+                            <Icon size={28} icon={wrench} />
+                          </span>
+                          {resume.project}
+                        </div>
+                      </h2>
+                      <p dangerouslySetInnerHTML={{ __html: resume.projectContent }} />
+                    </div>
+                    <div className={styles.section}>
+                      <h2>
+                        <div className={styles.thumbsOUp}>
+                          <span>
+                            <Icon size={28} icon={thumbsOUp} />
+                          </span>
+                          {resume.skill}
+                        </div>
+                      </h2>
+                      <p dangerouslySetInnerHTML={{ __html: resume.skillContent }} />
+                    </div>
+                    <div className={styles.section}>
+                      <h2>
+                        <div className={styles.trophy}>
+                          <span>
+                            <Icon size={28} icon={trophy} />
+                          </span>
+                          {resume.honors}
+                        </div>
+                      </h2>
+                      <p dangerouslySetInnerHTML={{ __html: resume.honorsContent }} />
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return <h2>Loading profile...</h2>;
+};
+
+const Resume = gql`
+ query resumes {
+  resumes {
+    name
+    profilepic {
+      url
+    }
+    introduction
+    education
+    educationContent
+    skill
+    skillContent
+    project
+    projectContent
+    honors
+    honorsContent
+    work
+    workContent
+  }
+}
+`;
+
+About.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default graphql(Resume)(About);
--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const API = process.env.REACT_APP_GRAPHCMS_API;
+const API = 'https://api-useast.graphcms.com/v1/cjl078g2y05mm01bnn407e40z/master';
 
 const client = new ApolloClient({
- link: new HttpLink({ uri: API }),
- cache: new InMemoryCache()
+  link: new HttpLink({ uri: API }),
+  cache: new InMemoryCache()
 });
 ReactDOM.render(
- <ApolloProvider client={client}>
-  <App />
- </ApolloProvider>,
-document.getElementById('root'));
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
-
