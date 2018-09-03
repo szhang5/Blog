887428940e9e7dc394a52a809664f8e14a6bebdd
szhang5 <shiyun.zhang.zoey@gmail.com>
2018-09-02 21:21:28 -0400

    Search component

--- src/components/SearchBlog.js
+++ src/components/SearchBlog.js
@@ -1,17 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { debounce, map } from 'lodash';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+import { withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withRouter } from 'react-router-dom';
 import styles from './styles/searchBlog.css';
 
-const SearchBlog = () => {
-  return (
-    <div className={styles.search}>
-      <div className={styles.searchDiv}>
-        Search
+
+class SearchBlog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { options: [] };
+    // This binding is necessary to make `this` work in the callback
+    this.getOptions = this.getOptions.bind(this);
+    this.handleOnSelect = this.handleOnSelect.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.debounceGetOptions = debounce(this.getOptions, 300);
+  }
+
+  getOptions(input) {
+    if (input.length < 3) {
+      return [];
+    }
+    return this.props.client.query({
+      query: gql`
+        query Posts($slug: String!) {
+          posts( where: {
+            title_contains: $slug
+            status: PUBLISHED
+          }){
+            createdAt
+            id
+            slug
+            title
+            content
+            image {
+             url
+            }
+          }
+        }`,
+      variables: { slug: input },
+    }).then((response) => {
+      const posts = map(response.data.posts, post => {
+        return {
+          value: post.slug, label: post.title
+        };
+      });
+      this.setState({
+        options: posts
+      });
+    });
+  }
+
+  handleInputChange(input) {
+    this.debounceGetOptions(input);
+  }
+
+  handleOnSelect({ value }) {
+    if (!value) {
+      return null;
+    }
+    return this.props.history.push(`/post/${value}`);
+  }
+
+  render() {
+    return (
+      <div className={styles.search}>
+        <div className={styles.searchDiv}>
+          Search
+        </div>
+        <div className={styles.searchBox}>
+          <Select
+            onInputChange={this.handleInputChange}
+            onChange={this.handleOnSelect}
+            options={this.state.options}
+            placeholder="Search"
+          />
+        </div>
       </div>
-      <div className={styles.searchBox}>
-        <input type="text" />
-      </div>
-    </div>
-  );
+    );
+  }
+}
+
+
+SearchBlog.propTypes = {
+  client: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
-export default SearchBlog;
+export default withApollo(withRouter(SearchBlog));
