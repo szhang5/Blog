b61e2bc7d5d22398d8a157f9d6abddc10c180b86
gaotangshen <gshen01@students.poly.edu>
2018-09-02 22:59:41 -0400

    add month and year to landing page

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { forEach, map } from 'lodash';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import PropTypes from 'prop-types';
@@ -11,37 +12,69 @@ import gridStyles from '../assets/styles/grid.css';
 import styles from './styles/landing.css';
 
 
+const getPostsByMonth = (posts) => {
+  const results = {};
+  forEach(posts, post => {
+    const monthYear = moment(post.createdAt).format('YYYY-MM');
+    if (monthYear in results) {
+      results[monthYear].push(post);
+    } else {_
+      results[monthYear] = [post];
+    }
+  })
+  const keys = Object.keys(results).reverse();
+  return { keys, results };
+};
+
 const Landing = ({ data: { loading, posts } }) => {
   if (!loading) {
+    const { keys, results: postsByMonth } = getPostsByMonth(posts);
     return (
       <div className={[gridStyles.col, gridStyles['span-3-of-4']].join(' ')}>
         <div className={styles.landingDiv}>
           <Header />
           <div className={styles.content}>
-            {posts.map(post => (
-              <div className={gridStyles.row} key={post.id}>
-                <div className={[gridStyles.col, gridStyles['span-1-of-4']].join(' ')}>
-                  <article className={styles.rightContent}>
-                    <h4>{moment(post.createdAt).format('dddd Do')}</h4>
-                  </article>
-                </div>
-                <div className={[gridStyles.col, gridStyles['span-3-of-4']].join(' ')}>
-                  <article className={styles.leftContent} key={post.id}>
-                    <h2>{post.title}</h2>
-                    <div className={styles.socialLinks}>
-                      <div className={styles.logoTag}>
-                        <span>{post.category.name}</span>
-                        <Icon size={18} icon={tag} />
-                      </div>
+            {
+              map(keys, monthYear => {
+                const posts = postsByMonth[monthYear];
+                return (
+                  <div key={monthYear}>
+                    <div className={gridStyles.row}>
+                      <div className={[gridStyles.col, gridStyles['span-1-of-12']].join(' ')} />
+                      <div className={[gridStyles.col, gridStyles['span-11-of-12']].join(' ')}>
+                        <div className={styles.date}>{moment(monthYear).format('MMMM YYYY')}</div>
+                      </div> 
                     </div>
-                    <p className={styles.ellipsis} dangerouslySetInnerHTML={{ __html: post.content }} />
-                    <Link to={`/post/${post.slug}`} style={{ textDecoration: 'none' }}>
-                      <button className={styles.btn} type="button">Read More</button>
-                    </Link>
-                  </article>
-                </div>
-              </div>
-            ))}
+                    {
+                      map(posts, post => {
+                        return (
+                          <div className={gridStyles.row} key={post.id}>
+                            <div className={[gridStyles.col, gridStyles['span-1-of-4']].join(' ')}>
+                              <article className={styles.rightContent}>
+                                <h4>{moment(post.createdAt).format('dddd Do')}</h4>
+                              </article>
+                            </div>
+                            <div className={[gridStyles.col, gridStyles['span-3-of-4']].join(' ')}>
+                              <article className={styles.leftContent} key={post.id}>
+                                <h2>{post.title}</h2>
+                                <div className={styles.socialLinks}>
+                                  <div className={styles.logoTag}>
+                                    <span>{post.category.name}</span>
+                                    <Icon size={18} icon={tag} />
+                                  </div>
+                                </div>
+                                <p className={styles.ellipsis} dangerouslySetInnerHTML={{ __html: post.content }} />
+                                <Link to={`/post/${post.slug}`} style={{ textDecoration: 'none' }}>
+                                  <button className={styles.btn} type="button">Read More</button>
+                                </Link>
+                              </article>
+                            </div>
+                          </div>);
+                      })
+                    }
+                  </div>
+                )})
+              }
           </div>
         </div>
       </div>
